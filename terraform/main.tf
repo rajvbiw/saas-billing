af43b91c04d9844829f10785b488335e86af79e8
rajbirari800 <rajbirari800@gmail.com>
2026-06-20 07:58:43 +0530

    Fix: Upgrade EKS worker nodes to t3.small to resolve pod limits

--- terraform/main.tf
+++ terraform/main.tf
@@ -225,7 +225,7 @@ resource "aws_eks_node_group" "main" {
     min_size     = 1
   }
 
-  instance_types = ["t3.micro"]
+  instance_types = ["t3.small"]
 
   depends_on = [
     aws_iam_role_policy_attachment.eks_node_policy,
